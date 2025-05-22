33d6ab367d06123b5a99216ee190046ca6b11890
joins <12934782+joinse@user.noreply.gitee.com>
2025-05-22 10:24:33 +0800

    修改ip

--- src/common.rs
+++ src/common.rs
@@ -910,7 +910,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://101.43.167.126:21114".to_owned()
+    "http://140.143.203.41:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
